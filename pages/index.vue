735bb30962b683d7787acdf18e90faec86247f06
flkl192b <Florian.kluge@mailbox.tu-dresden.de>
2021-12-22 21:31:46 +0100

    move index to temp

--- pages/index.vue
+++ pages/index.vue
@@ -1,43 +1,23 @@
 <template>
   <div class="wrapper">
-    <Header />
-    <div class="content">
-      <div class="card">
-        <h1 class="fira">Playground</h1>
-      </div>
-    </div>
+    <ClientOnly>
+      <Editor />
+    </ClientOnly>
   </div>
 </template>
 
 <script lang="ts" type="module">
 import Vue from 'vue'
-import Header from '~/components/Header/Header.vue'
 
 export default Vue.extend({
-  name: 'IndexPage',
+  name: 'Playground',
 })
 </script>
 <style scoped>
 .wrapper {
   width: 100%;
   height: 100%;
-}
-
-.content {
-  width: 100%;
-  height: auto;
-  display: flex;
-  justify-content: center;
-  margin-top: 25px;
-}
-
-.card {
-  padding: 10px 25px;
-  width: auto;
-  height: auto;
-  border: solid 1px rgb(90, 90, 90);
-  align-content: center;
-  text-align: center;
-  border-radius: 5px;
+  /* height: 100%; */
+  /* border: solid 2px red; */
 }
 </style>
