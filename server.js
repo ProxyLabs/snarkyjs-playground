d7b8af94d04a20396eb72bccdc72460c7b3c17d9
flkl192b <Florian.kluge@mailbox.tu-dresden.de>
2021-12-24 15:35:52 +0100

    add simple server

---
+++ server.js
@@ -0,0 +1,25 @@
+import express from 'express'
+import process from 'process'
+const app = express()
+const PORT = 3000
+
+app.use((req, res, next) => {
+  res.append('X-Frame-Options', 'ALLOWALL')
+  res.append('Access-Control-Allow-Origin', '*')
+  res.append('Access-Control-Allow-Methods', 'GET')
+  res.append(
+    'Access-Control-Allow-Headers',
+    'Origin, X-Requested-With, Content-Type, Accept'
+  )
+  res.append('Cross-Origin-Opener-Policy', 'same-origin')
+  res.append('Cross-Origin-Embedder-Policy', 'require-corp')
+  res.append('Cross-Origin-Opener-Policy-Report-Only', 'same-origin')
+  res.append('Cross-Origin-Embedder-Policy-Report-Only', 'require-corp')
+  next()
+})
+
+app.use(express.static('dist'))
+
+app.listen(PORT, () =>
+  console.log(`Server listening on port: ${PORT} with PID ${process.pid}`)
+)
