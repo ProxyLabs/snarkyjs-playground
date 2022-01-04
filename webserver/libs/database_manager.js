4b7bd6600cd2fbde0ec034016a602218ce2068c0
Florian <trivoplays@gmail.com>
2022-01-04 14:11:56 +0100

    change database credentials

--- webserver/libs/database_manager.js
+++ webserver/libs/database_manager.js
@@ -16,8 +16,8 @@ CREATE TABLE projects (
 function getClient() {
   const client = new Client({
     host: process.env.PG_HOST,
-    user: process.env.PG_USER,
-    database: process.env.PG_DATABASE,
+    user: process.env.PG_USER_SNARKYJS_PLAYGROUND,
+    database: 'snarkyjs_playground',
     port: process.env.PG_PORT,
     password: process.env.PG_PASSWORD,
   })
